f906ebdda7b6ae379292cbd476128f32f50ac0b8
Stefan Manenschijn <s.manenschijn@epartment.nl>
2020-12-14 14:09:11 +0100

    fixed missing parts

--- Block/Script.php
+++ Block/Script.php
@@ -8,4 +8,9 @@ class Script extends \Magento\Framework\View\Element\Template
     {
         return $this->getUrl('pricing');
     }
+
+    public function isModuleEnabledForStore()
+    {
+        return $this->_scopeConfig->getValue('conneqt_specialprices/settings/active', \Magento\Store\Model\ScopeInterface::SCOPE_WEBSITE) !== "0";
+    }
 }
